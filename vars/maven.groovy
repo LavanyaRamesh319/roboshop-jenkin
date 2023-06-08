50ba97adc134e32e9c88f25c96e9d7cfffd68a94
Raghu K <raghuk.vit@gmail.com>
2023-06-08 07:27:00 +0530

    Update roboshop-jenkins/

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -22,14 +22,16 @@ def call() {
 
       stage('Code Quality') {
         steps {
-          sh 'ls -l'
-          sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.8.27:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+//          sh 'ls -l'
+//          sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.8.27:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+          sh 'echo COde Quality'
         }
       }
 
       stage('Unit Test Cases') {
         steps {
           sh 'echo Unit tests'
+          sh 'mvn test'
         }
       }
 
