438cd86940455e22e2d0c7b421163a9935c447cd
Raghu K <raghuk.vit@gmail.com>
2023-06-09 07:00:10 +0530

    Update roboshop-jenkins/

--- vars/python.groovy
+++ vars/python.groovy
@@ -41,6 +41,19 @@ def call() {
         }
       }
 
+      stage('Release Application') {
+        when {
+          expression {
+            env.TAG_NAME ==~ ".*"
+          }
+        }
+        steps {
+          sh 'echo $TAG_NAME >VERSION'
+          sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt VERSION'
+          sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.149:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+        }
+      }
+
 
     }
 
