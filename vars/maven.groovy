49308b9d2ed9d37eb142ce1096bac4d40ef4a5ed
Raghu K <raghuk.vit@gmail.com>
2023-06-10 07:16:10 +0530

    Update roboshop-jenkins/

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -11,6 +11,10 @@ def call() {
       ansiColor('xterm')
     }
 
+    environment {
+      NEXUS = credentials('NEXUS')
+    }
+
 
     stages {
 
@@ -57,7 +61,7 @@ def call() {
           sh 'mvn package ; cp target/${component}-1.0.jar ${component}.jar'
           sh 'echo $TAG_NAME >VERSION'
           sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
-          sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.149:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+          sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.149:8081/repository/${component}/${component}-${TAG_NAME}.zip'
         }
       }
 
--- vars/python.groovy
+++ vars/python.groovy
@@ -11,6 +11,9 @@ def call() {
       ansiColor('xterm')
     }
 
+    environment {
+      NEXUS = credentials('NEXUS')
+    }
 
     stages {
 
@@ -50,7 +53,7 @@ def call() {
         steps {
           sh 'echo $TAG_NAME >VERSION'
           sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt VERSION'
-          sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.149:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+          sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.149:8081/repository/${component}/${component}-${TAG_NAME}.zip'
         }
       }
 
